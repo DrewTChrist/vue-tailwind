36a58cb6d34961cb42313f95d64bca67ae171629
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-10-14 21:41:37 +0000

    Add 404 view and catch all route

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import NotFoundView from '../views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,7 +14,12 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
-    }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFoundView
+    },
   ]
 })
 
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="font-bold text-4xl">404 Not Found</h1>
+  </div>
+</template>
