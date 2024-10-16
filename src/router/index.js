ce26476dd21cc79bace55738ba57299c554a2b61
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2024-10-16 20:37:46 +0000

    Lazy load routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import NotFoundView from '../views/NotFoundView.vue'
+
+const HomeView = import('../views/HomeView.vue')
+const AboutView = import('../views/AboutView.vue')
+const NotFoundView = import('../views/NotFoundView.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,17 +10,17 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      component: () => HomeView,
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      component: () => AboutView,
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'NotFound',
-      component: NotFoundView,
+      component: () => NotFoundView,
     },
   ],
 })
